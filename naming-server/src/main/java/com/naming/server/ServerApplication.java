c54ffaaae72e449f093c0742078e37698c14a7ae
Rodrigo Rech <rodrigorech92@gmail.com>
2022-07-13 14:05:53 -0300

    Feat: naming-server created

---
+++ naming-server/src/main/java/com/naming/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.naming.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
